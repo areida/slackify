7c6d431b973e52c7849e8581c06edc10566f7692
Andrew Reida <andrew@syn0.com>
2014-06-29 00:26:33 -0700

    Only  autostarting in development

--- application/bootstrap.js
+++ application/bootstrap.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var Application = require('./application');
+var config      = require('config');
 
 // Polyfill for str.contains
 if ( ! String.prototype.contains ) {
@@ -12,3 +13,8 @@ if ( ! String.prototype.contains ) {
 }
 
 window.app = new Application();
+
+if (config.autostart)
+{
+	window.app.start({name : 'Developer'});	
+}
